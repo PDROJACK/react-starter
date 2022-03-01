b483ee4b83c07cc9cd416f795cf7f100cdb4dd2d
pdrojack <singhpd75@gmail.com>
2022-03-02 02:00:08 +0530

    added table

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./screens/Homepage";
+import MoviePage from "./screens/MoviePage";
+import UserPage from "./screens/UserPage";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Homepage />} />
+            <Route path="products" element={<MoviePage />}>
+              <Route path="movie" element={<Homepage />} />
+              <Route path="new" element={<UserPage />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/screens/Homepage.tsx
+++ src/screens/Homepage.tsx
@@ -1,38 +1,60 @@
-import React from 'react'
-import Grid from '@mui/material/Grid';
-import Header from '../components/Header';
-import CineCard from '../components/CineCard';
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Header from "../components/Header";
+import CineCard from "../components/CineCard";
+import HeroImage from "../components/HeroImage";
+import { Typography } from "@mui/material";
+import { AutoAwesome } from "@mui/icons-material";
+import Products from "../features/product/Products";
 
 function Homepage() {
-    return (
-        <Grid container direction="column">
-            <Grid item>
-                <Header/>
-            </Grid>
-
-            <Grid item>
-                banner
-                {/* <Bannar/> */}
-            </Grid>
-
-            <Grid item xs={12} md={11} lg={10}>
-                <Grid direction="row">
-                    Movie
-                    <CineCard/>
-                </Grid>
-
-                <Grid direction="row">
-                    TV
-                    <CineCard/>
-                </Grid>
-            </Grid>
-
-            <Grid item>
-                footer
-            </Grid>
+  return (
+    <Grid container direction="column">
+      
+      <Grid item container>
+        <Grid item sm={12}>
+          <Header />
         </Grid>
-    )
+      </Grid>
+
+      {/* <Grid item container>
+        <Grid item sm={12}>
+          <HeroImage />
+        </Grid>
+      </Grid> */}
+
+      <Grid item container>
+        <Grid item lg={1} xs={0}/>
+        
+        <Grid item lg={8} direction="row">
+
+            <Products />
+
+        </Grid>
+
+        <Grid item lg={2} direction="row">
+
+            <Products />
+
+        </Grid>
+            
+        <Grid item lg={1} xs={0}/>
+
+
+        {/* <Grid item sm={10}>
+          <Grid direction="row" lg={8}>
+            <Typography variant="h5" component="h5">
+              TV
+            </Typography>
+            <CineCard />
+          </Grid>
+        </Grid> */}
+
+      </Grid>
+
+      <Grid item>footer</Grid>
+    </Grid>
+  );
 }
 
-export default Homepage
-
+export default Homepage;
